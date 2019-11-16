dd7af312379ee88ab8fb175056613df479c07dae
Nuria P <guaupelu@hotmail.com>
2019-11-16 14:52:08 +0100

    Ejercicios basicos python

--- python_intro.py
+++ python_intro.py
@@ -1,3 +1,37 @@
-if 3 > 2:
-    print('It works!')
-    
+volume = 57
+
+#Cambiar el volumen si esta muy alto o muy bajo
+if volume < 20:
+    print("It's kinda quiet.")
+elif 20 <= volume < 40:
+    print("It's nice for background music")
+elif 40 <= volume < 60:
+    print("Perfect, I can hear all the details")
+elif 60 <= volume < 80:
+    print("Nice for parties")
+elif 80 <= volume < 100:
+    print("A bit loud!")
+else:
+    print("Me duelen las orejas! :(")
+
+def hi(name):
+    if name == 'Ola':
+        print('Hi Ola!')
+    elif name == 'Sonja':
+        print('Hi Sonja!')
+    else:
+        print('Hi anonymous!')
+hi('Maria')
+
+
+def hi(name):
+    print('Hi ' + name + '!')
+
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+for name in girls:
+    hi(name)
+    print('Next girl')
+
+for i in range(1, 6):
+    print(i)
+
